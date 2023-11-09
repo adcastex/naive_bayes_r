dc76eb63d50fcb09b209f11de5600fdd4388af03
victorsigogneau <victor.sigogneau19@gmail.com>
2023-11-09 13:04:38 +0100

    Update Fonction_Gaussienne_Proba.R

--- Victor/Fonction_Gaussienne_Proba.R
+++ Victor/Fonction_Gaussienne_Proba.R
@@ -1,23 +1,20 @@
 data("iris")
 colnames(iris)[colnames(iris) == "Species"] <- "Class"
 
-#fonction GAUSSIAN
+
+
+######################################################
+# Fonction qui retourne la valeur proba gausienne   ##
+######################################################
 pdf_gaussian <- function(x, mean, std_dev) {
   exponent <- -((x - mean) ^ 2) / (2 * (std_dev ^ 2))
   coefficient <- 1 / (std_dev * sqrt(2 * pi))
   return(coefficient * exp(exponent))
 }
 
-# Nouvelles données à classer
-new_data <- data.frame(
-  Sepal.Length = c(5.1, 6.0),
-  Sepal.Width = c(3.5, 2.7),
-  Petal.Length = c(1.4, 4.1),
-  Petal.Width = c(0.2, 1.3)
-)
-
-
-# Fonction pour calculer les probabilités de classe
+######################################################
+# Fonction pour calculer les probabilités de classe ##
+######################################################
 calculate_class_probabilities <- function(new_data, training_data) {
   # Estimation des moyennes et des écarts-types pour chaque classe
   means <- aggregate(. ~ Class, data = training_data, FUN = mean)
@@ -46,8 +43,36 @@ calculate_class_probabilities <- function(new_data, training_data) {
   return(class_probabilities)
 }
 
+######################################################
+# Fonction pour prédire la classe d'appartennace    ##
+######################################################
+predict_class <- function(class_probabilities, training_data) {
+  predicted_classes <- apply(class_probabilities, 1, which.max)
+  class_names <- unique(training_data$Class)
+  predicted_classes <- class_names[predicted_classes]
+  return(predicted_classes)
+}
+
+######################################################
+#                 Phase de tests                    ##
+######################################################
+# Nouvelles données à classer
+new_data <- data.frame(
+  Sepal.Length = c(5.1, 6.0),
+  Sepal.Width = c(3.5, 2.7),
+  Petal.Length = c(1.4, 4.1),
+  Petal.Width = c(0.2, 1.3)
+)
+
 # Appel de la fonction avec les nouvelles données
 class_probabilities <- calculate_class_probabilities(new_data, iris)
 print(class_probabilities)
+
+# Appel de la fonction pour obtenir les classes prédites
+predicted_classes <- predict_class(class_probabilities, iris)
+
+# Affichage des classes prédites
+print(predicted_classes)
+
 
 
